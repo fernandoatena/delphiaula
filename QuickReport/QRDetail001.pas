77b1acabd64ca529527fb6346f6a140e0d2327db
jvds <joaovitor@atenainformatica.com.br>
2016-07-03 11:37:53 -0300

    create QRDetail001

---
+++ QuickReport/QRDetail001.pas
@@ -0,0 +1,23 @@
+unit QRDetail001;
+
+interface
+
+uses Windows, SysUtils, Messages, Classes, Graphics, Controls,
+  StdCtrls, ExtCtrls, Forms, QuickRpt, QRCtrls;
+
+type
+  TFrmQRDetail001 = class(TQuickRep)
+  private
+
+  public
+
+  end;
+
+var
+  FrmQRDetail001: TFrmQRDetail001;
+
+implementation
+
+{$R *.DFM}
+
+end.
--- QuickReport/QuickReportPJ.dpr
+++ QuickReport/QuickReportPJ.dpr
@@ -3,7 +3,8 @@ program QuickReportPJ;
 uses
   Forms,
   QuickReport in 'QuickReport.pas' {FrmQuickReport},
-  DataModuleConexao in 'DataModuleConexao.pas' {DMConexao: TDataModule};
+  DataModuleConexao in 'DataModuleConexao.pas' {DMConexao: TDataModule},
+  QRDetail001 in 'QRDetail001.pas' {FrmQRDetail001: TQuickRep};
 
 {$R *.res}
 
@@ -11,5 +12,6 @@ begin
   Application.Initialize;
   Application.CreateForm(TFrmQuickReport, FrmQuickReport);
   Application.CreateForm(TDMConexao, DMConexao);
+  Application.CreateForm(TFrmQRDetail001, FrmQRDetail001);
   Application.Run;
 end.
